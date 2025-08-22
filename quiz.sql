9acc1bb3e6aa1a8618b2eae68f25ec558bbed41f
Victor Dalbon <135763462+Victor-Dalbon@users.noreply.github.com>
2025-08-22 17:37:40 -0300

    Update quiz.sql

--- quiz.sql
+++ quiz.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `quiz` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `quiz`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
--- Host: mainline.proxy.rlwy.net    Database: railway
+-- Host: 127.0.0.1    Database: quiz
 -- ------------------------------------------------------
--- Server version	9.4.0
+-- Server version	8.0.43
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
