457d8049f9b93ac0c6bc556afc9498f6d1515418
aluno <lucasgabrielpena123@gmail.com>
2025-09-12 11:59:27 -0300

    ``` Atualizaçao estrutura do Banco de Dados ```

--- quiz.sql
+++ quiz.sql
@@ -1,219 +1,298 @@
-CREATE DATABASE  IF NOT EXISTS `quiz` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `quiz`;
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: quiz
--- ------------------------------------------------------
--- Server version	8.0.43
+CREATE database if not exists app_db;
+use app_db;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `alternativas`
---
 
-DROP TABLE IF EXISTS `alternativas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `alternativas` (
-  `idAlternativa` int NOT NULL AUTO_INCREMENT,
-  `texto` varchar(255) NOT NULL,
-  `estaCorreta` tinyint NOT NULL DEFAULT '0',
-  `idQuestao` int NOT NULL,
-  PRIMARY KEY (`idAlternativa`),
-  KEY `FK_8b71b4dbda900f09fa4f459b492` (`idQuestao`),
-  CONSTRAINT `FK_8b71b4dbda900f09fa4f459b492` FOREIGN KEY (`idQuestao`) REFERENCES `questoes` (`idQuestao`) ON DELETE CASCADE
+CREATE TABLE if not exists curso (
+  idCurso INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `alternativas`
---
-
-LOCK TABLES `alternativas` WRITE;
-/*!40000 ALTER TABLE `alternativas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alternativas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `curso`
---
-
-DROP TABLE IF EXISTS `curso`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `curso` (
-  `idCurso` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(30) NOT NULL,
-  PRIMARY KEY (`idCurso`)
+CREATE TABLE if not exists curso_log (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  acao VARCHAR(10) NOT NULL,
+  idCurso INT DEFAULT NULL,
+  nome VARCHAR(30) DEFAULT NULL,
+  data_acao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `curso`
---
 
-LOCK TABLES `curso` WRITE;
-/*!40000 ALTER TABLE `curso` DISABLE KEYS */;
-/*!40000 ALTER TABLE `curso` ENABLE KEYS */;
-UNLOCK TABLES;
+DELIMITER //
+CREATE TRIGGER log_after_insert_curso
+AFTER INSERT ON curso
+FOR EACH ROW
+BEGIN
+  INSERT INTO curso_log (acao, idCurso, nome)
+  VALUES ('INSERT', NEW.idCurso, NEW.nome);
+END //
+DELIMITER ;
 
---
--- Table structure for table `disciplina`
---
 
-DROP TABLE IF EXISTS `disciplina`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `disciplina` (
-  `idDisciplina` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(20) NOT NULL,
-  `idCurso` int NOT NULL,
-  PRIMARY KEY (`idDisciplina`),
-  KEY `FK_2645acc90d7f4141357ae25f14b` (`idCurso`),
-  CONSTRAINT `FK_2645acc90d7f4141357ae25f14b` FOREIGN KEY (`idCurso`) REFERENCES `curso` (`idCurso`) ON DELETE CASCADE
+DROP TRIGGER IF EXISTS log_after_insert_disciplina;
+DROP TABLE IF EXISTS disciplina_log;
+DROP TABLE IF EXISTS disciplina;
+
+CREATE TABLE disciplina (
+  idDisciplina INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  idCurso INT NOT NULL,
+  CONSTRAINT FK_disciplina_curso FOREIGN KEY (idCurso) REFERENCES curso (idCurso) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `disciplina`
---
-
-LOCK TABLES `disciplina` WRITE;
-/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
-/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `questionario`
---
-
-DROP TABLE IF EXISTS `questionario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `questionario` (
-  `idQuestionario` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `idDisciplina` int NOT NULL,
-  PRIMARY KEY (`idQuestionario`),
-  KEY `FK_131e26ffd533e2d1f10666fe41a` (`idDisciplina`),
-  CONSTRAINT `FK_131e26ffd533e2d1f10666fe41a` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`idDisciplina`)
+CREATE TABLE disciplina_log (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  acao VARCHAR(10) NOT NULL,
+  idDisciplina INT DEFAULT NULL,
+  nome VARCHAR(50) DEFAULT NULL,
+  idCurso INT DEFAULT NULL,
+  data_acao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `questionario`
---
 
-LOCK TABLES `questionario` WRITE;
-/*!40000 ALTER TABLE `questionario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `questionario` ENABLE KEYS */;
-UNLOCK TABLES;
+DELIMITER //
+CREATE TRIGGER log_after_insert_disciplina
+AFTER INSERT ON disciplina
+FOR EACH ROW
+BEGIN
+  INSERT INTO disciplina_log (acao, idDisciplina, nome, idCurso)
+  VALUES ('INSERT', NEW.idDisciplina, NEW.nome, NEW.idCurso);
+END //
+DELIMITER ;
 
---
--- Table structure for table `questoes`
---
 
-DROP TABLE IF EXISTS `questoes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `questoes` (
-  `idQuestao` int NOT NULL AUTO_INCREMENT,
-  `dificuldade` enum('FACIL','MEDIO','DIFICIL') NOT NULL,
-  `enunciado` varchar(255) NOT NULL,
-  `idDisciplina` int DEFAULT NULL,
-  PRIMARY KEY (`idQuestao`),
-  KEY `FK_40f1277dbe28c1ed240c37edbb5` (`idDisciplina`),
-  CONSTRAINT `FK_40f1277dbe28c1ed240c37edbb5` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`idDisciplina`) ON DELETE CASCADE
+DROP TABLE IF EXISTS questionario;
+CREATE TABLE questionario (
+  idQuestionario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  idDisciplina INT NOT NULL,
+  CONSTRAINT FK_questionario_disciplina FOREIGN KEY (idDisciplina) REFERENCES disciplina (idDisciplina)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `questoes`
---
 
-LOCK TABLES `questoes` WRITE;
-/*!40000 ALTER TABLE `questoes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `questoes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `resultado`
---
-
-DROP TABLE IF EXISTS `resultado`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `resultado` (
-  `idResultado` int NOT NULL AUTO_INCREMENT,
-  `tempoSegundos` int NOT NULL DEFAULT '0',
-  `dataExecucao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `idUsuario` int NOT NULL,
-  `idQuestionario` int NOT NULL,
-  PRIMARY KEY (`idResultado`),
-  KEY `FK_99020ec316d0e2bd895e751b276` (`idUsuario`),
-  KEY `FK_580d0386202c551a9498e25cb89` (`idQuestionario`),
-  CONSTRAINT `FK_580d0386202c551a9498e25cb89` FOREIGN KEY (`idQuestionario`) REFERENCES `questionario` (`idQuestionario`) ON DELETE CASCADE,
-  CONSTRAINT `FK_99020ec316d0e2bd895e751b276` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE
+DROP TABLE IF EXISTS questoes;
+CREATE TABLE questoes (
+  idQuestao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dificuldade ENUM('FACIL','MEDIO','DIFICIL') NOT NULL,
+  enunciado VARCHAR(255) NOT NULL,
+  idDisciplina INT DEFAULT NULL,
+  CONSTRAINT FK_questoes_disciplina FOREIGN KEY (idDisciplina) REFERENCES disciplina (idDisciplina) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `resultado`
---
 
-LOCK TABLES `resultado` WRITE;
-/*!40000 ALTER TABLE `resultado` DISABLE KEYS */;
-/*!40000 ALTER TABLE `resultado` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `idUsuario` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
-  `apelido` varchar(20) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  `admin` tinyint NOT NULL DEFAULT '0',
-  `pontuacao` int NOT NULL DEFAULT '0',
-  `criadoEm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idUsuario`),
-  UNIQUE KEY `IDX_446adfc18b35418aac32ae0b7b` (`email`),
-  UNIQUE KEY `IDX_53e4306997d1df791f4db3245b` (`apelido`),
-  UNIQUE KEY `IDX_0ffa236b3322ba9262d56936bb` (`email`,`apelido`)
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+  idUsuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(50) NOT NULL,
+  apelido VARCHAR(20) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  admin TINYINT NOT NULL DEFAULT 0,
+  pontuacao_geral INT NOT NULL DEFAULT 0,
+  criadoEm TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY IDX_email (email),
+  UNIQUE KEY IDX_apelido (apelido)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `usuarios`
---
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+DROP TABLE IF EXISTS resultado;
+CREATE TABLE resultado (
+  idResultado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tempoSegundos INT NOT NULL DEFAULT 0,
+  dataExecucao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  idUsuario INT NOT NULL,
+  idQuestionario INT NOT NULL,
+  CONSTRAINT FK_resultado_usuario FOREIGN KEY (idUsuario) REFERENCES usuarios (idUsuario) ON DELETE CASCADE,
+  CONSTRAINT FK_resultado_questionario FOREIGN KEY (idQuestionario) REFERENCES questionario (idQuestionario) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-22 17:23:20
+DROP TABLE IF EXISTS alternativas;
+CREATE TABLE alternativas (
+  idAlternativa INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  texto VARCHAR(255) NOT NULL,
+  estaCorreta TINYINT NOT NULL DEFAULT 0,
+  idQuestao INT NOT NULL,
+  CONSTRAINT FK_alternativas_questao FOREIGN KEY (idQuestao) REFERENCES questoes (idQuestao) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS respostas_usuario;
+CREATE TABLE respostas_usuario (
+  idResposta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  idUsuario INT NOT NULL,
+  idQuestao INT NOT NULL,
+  idAlternativa INT NOT NULL,
+  correta TINYINT NOT NULL DEFAULT 0,
+  idResultado INT NOT NULL,
+  CONSTRAINT FK_respostas_usuario_idUsuario FOREIGN KEY (idUsuario) REFERENCES usuarios (idUsuario) ON DELETE CASCADE,
+  CONSTRAINT FK_respostas_usuario_idQuestao FOREIGN KEY (idQuestao) REFERENCES questoes (idQuestao) ON DELETE CASCADE,
+  CONSTRAINT FK_respostas_usuario_idAlternativa FOREIGN KEY (idAlternativa) REFERENCES alternativas (idAlternativa) ON DELETE CASCADE,
+  CONSTRAINT FK_respostas_usuario_idResultado FOREIGN KEY (idResultado) REFERENCES resultado (idResultado) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS usuarios_questionarios;
+CREATE TABLE usuarios_questionarios ( 
+  idUsuario INT NOT NULL,
+  idQuestionario INT NOT NULL,
+  total_acertos INT DEFAULT 0,
+  total_erros INT DEFAULT 0,
+  tempo_medio_segundos FLOAT DEFAULT 0,
+  tentativas INT DEFAULT 0,
+  PRIMARY KEY (idUsuario, idQuestionario),
+  CONSTRAINT FK_usuarios_questionarios_usuario FOREIGN KEY (idUsuario) REFERENCES usuarios (idUsuario) ON DELETE CASCADE,
+  CONSTRAINT FK_usuarios_questionarios_questionario FOREIGN KEY (idQuestionario) REFERENCES questionario (idQuestionario) ON DELETE CASCADE
+)
+
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS HistoricoResultadosUsuario //
+
+CREATE PROCEDURE HistoricoResultadosUsuario(IN p_idUsuario INT)
+BEGIN
+    SELECT 
+        r.dataExecucao AS data,
+        COUNT(CASE WHEN ru.correta = 1 THEN 1 END) AS pontuacao,
+        r.tempoSegundos AS tempo,
+        q.nome AS questionario
+    FROM resultado r
+    JOIN questionario q ON r.idQuestionario = q.idQuestionario
+    LEFT JOIN respostas_usuario ru ON ru.idResultado = r.idResultado
+    WHERE r.idUsuario = p_idUsuario
+    GROUP BY r.idResultado, r.dataExecucao, r.tempoSegundos, q.nome
+    ORDER BY r.dataExecucao DESC;
+END //
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS TopQuestoesRevisao //
+CREATE PROCEDURE TopQuestoesRevisao()
+BEGIN
+    SELECT 
+        q.idQuestao,
+        q.enunciado,
+        COUNT(ru.idResposta) AS vezes_errada
+    FROM questoes q
+    JOIN respostas_usuario ru 
+        ON q.idQuestao = ru.idQuestao 
+       AND ru.correta = 0
+    GROUP BY q.idQuestao, q.enunciado
+    ORDER BY vezes_errada DESC
+    LIMIT 10;
+END //
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS RegistrarResultQuestionario //
+CREATE PROCEDURE RegistrarResultQuestionario(
+    IN p_idUsuario INT,
+    IN p_idQuestionario INT,
+    IN p_tempoSegundos INT,
+    IN p_pontuacaoObtida INT
+)
+BEGIN
+    INSERT INTO resultado (tempoSegundos, idUsuario, idQuestionario)
+    VALUES (p_tempoSegundos, p_idUsuario, p_idQuestionario);
+
+    UPDATE usuarios
+    SET pontuacao = pontuacao + p_pontuacaoObtida
+    WHERE idUsuario = p_idUsuario;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+
+ALTER TABLE questionario
+ADD COLUMN click INT NOT NULL DEFAULT 0;
+
+
+DROP PROCEDURE IF EXISTS IncrementarClickQuestionario //
+CREATE PROCEDURE IncrementarClickQuestionario(IN p_idQuestionario INT)
+BEGIN
+    UPDATE questionario
+    SET click = click + 1
+    WHERE idQuestionario = p_idQuestionario;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS AlterarSenha //
+CREATE PROCEDURE AlterarSenha(IN p_idUsuario INT, IN p_novaSenha VARCHAR(255))
+BEGIN
+    UPDATE usuarios
+    SET senha = p_novaSenha
+    WHERE idUsuario = p_idUsuario;
+END //
+
+DELIMITER ;
+
+CREATE OR REPLACE VIEW ranking AS
+SELECT 
+    u.idUsuario,
+     u.pontuacao_geral,
+    RANK() OVER (ORDER BY  u.pontuacao_geral DESC) AS posicao
+FROM usuarios u order by posicao limit 10;
+
+DELIMITER //
+
+DELIMITER //
+
+DROP TRIGGER IF EXISTS after_insert_resultado //
+
+CREATE TRIGGER after_insert_resultado
+AFTER INSERT ON resultado
+FOR EACH ROW
+BEGIN
+    DECLARE totalAcertos INT;
+    DECLARE totalErros INT;
+    DECLARE tempoMedio FLOAT;
+    DECLARE tentativas INT;
+
+    
+    SELECT COUNT(*) INTO totalAcertos
+    FROM respostas_usuario
+    WHERE idUsuario = NEW.idUsuario AND idQuestao IN (
+        SELECT idQuestao FROM questoes WHERE idDisciplina = (
+            SELECT idDisciplina FROM questionario WHERE idQuestionario = NEW.idQuestionario
+        )
+    ) AND correta = 1;
+
+   
+    SELECT COUNT(*) INTO totalErros
+    FROM respostas_usuario
+    WHERE idUsuario = NEW.idUsuario AND idQuestao IN (
+        SELECT idQuestao FROM questoes WHERE idDisciplina = (
+            SELECT idDisciplina FROM questionario WHERE idQuestionario = NEW.idQuestionario
+        )
+    ) AND correta = 0;
+
+
+    SELECT AVG(tempoSegundos) INTO tempoMedio
+    FROM resultado
+    WHERE idUsuario = NEW.idUsuario AND idQuestionario = NEW.idQuestionario;
+
+    SELECT COUNT(*) INTO tentativas
+    FROM resultado
+    WHERE idUsuario = NEW.idUsuario AND idQuestionario = NEW.idQuestionario;
+
+
+    INSERT INTO usuarios_questionarios (idUsuario, idQuestionario, total_acertos, total_erros, tempo_medio_segundos, tentativas)
+    VALUES (NEW.idUsuario, NEW.idQuestionario, totalAcertos, totalErros, tempoMedio, tentativas)
+    ON DUPLICATE KEY UPDATE
+        total_acertos = totalAcertos,
+        total_erros = totalErros,
+        tempo_medio_segundos = tempoMedio,
+        tentativas = tentativas;
+
+
+    UPDATE usuarios
+    SET pontuacao_geral = pontuacao_geral + (
+        SELECT SUM(correta) FROM respostas_usuario WHERE idResultado = NEW.idResultado
+    )
+    WHERE idUsuario = NEW.idUsuario;
+END //
+
+DELIMITER ;
